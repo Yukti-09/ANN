1e0f061eb1e32bf9019abc2543242f40c9635954
Yukti Khosla <44090430+Yukti-09@users.noreply.github.com>
2020-08-11 17:44:41 +0530

    Update Threshold.m

--- Activation Functions/Threshold.m
+++ Activation Functions/Threshold.m
@@ -1,6 +1,7 @@
 %Threshold function using inbuilt func
 x=-10:0.01:10;
 y=heaviside(x);
+figure(1);
 plot(x,y);
 xlabel('x');
 ylabel('y');
@@ -9,6 +10,7 @@ ylabel('y');
 x = -10:0.01:10;
 y = zeros(size(x));
 y(x>=0)=1;
+figure(2);
 plot(x,y);
 xlabel('x');
 ylabel('y');
